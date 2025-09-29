06ea5214f75c2f59d6483e467e7f04531edcf0c4
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2025-09-29 10:11:26 +0900

    Remove unused files

--- data2log.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-setopt numericglobsort
-
-for i in *.dat; do
-../decoder/decode-latency $i > $i:r.log
-done
-
--- result.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-setopt numericglobsort
-
-for i in *.log; do
-echo -n "$i:r "; cavr -s 2 $i
-done | awk '{print $1, $2*1000000, $4}'  > result
